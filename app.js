6e75c180a7221f479e5b40ada6c7b624966439c8
thydaduong <thydaduong7@gmail.com>
2019-12-27 19:36:57 +0700

    allow cors

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const axios = require("axios");
 require("dotenv").config();
 const express = require("express");
 const app = express();
-app.use(cors);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
